4897647e3de18b1664f95295099d423bf9186edd
Unknown <claudiu@megheaiulian.com>
2018-05-04 18:26:01 +0300

    plumtwo profile
    Kernel config
    
    config
    
    config

--- profiles/profile.nix
+++ profiles/profile.nix
@@ -3,6 +3,14 @@
   imports = [
     ./local.nix
     ./plumelo.nix
-    ../modules/services/X11/gnome3.nix 
+    ../modules/services/X11/kde.nix 
   ];
+  boot = {
+    kernelModules = [
+      "it87"
+    ];
+    extraModulePackages  = [pkgs.it87];
+    kernelPackages = pkgs.linux_4_17_gag3wifi;
+  }; 
+  services.xserver.videoDrivers = [ "amdgpu" ];
 }
